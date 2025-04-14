1e111213e8c15db6fb6435d4354e6134c4672918
김네모 <breadnko@gmail.com>
2025-04-15 01:51:52 +0900

    [week-1/home-work] TODO FORM, LIST 테스트코드

---
+++ zz.home-work/src/components/test/todo-form.test.tsx
@@ -0,0 +1,51 @@
+import { render, fireEvent, act } from '@testing-library/react';
+import { TodoForm } from '../todo-form';
+
+describe('todo form test', () => {
+
+  let todoFormText: HTMLElement;
+  let todoFormDeadline: HTMLElement;
+  let todoFormButton: HTMLElement;
+
+  beforeEach(() => {
+    renderTodoForm();
+  });
+
+  const renderTodoForm = () => {
+    const AppRenderResult = render(<TodoForm todos={[]} setTodos={vi.fn()} />);
+    const { getByTestId } = AppRenderResult
+
+    todoFormText = getByTestId('todo-form-text');
+    todoFormDeadline = getByTestId('todo-form-deadline');
+    todoFormButton = getByTestId('todo-form-button');
+  };
+
+  it('New Todo와 Deadline 입력하면 ADD 버튼이 활성화된다.', () => {
+    // WHEN
+    fireEvent.change(todoFormText, { target: { value: '파운드 자르기' } });
+    fireEvent.change(todoFormDeadline, { target: { value: '2025-04-14' } });
+
+    // THEN
+    expect(todoFormButton).toBeEnabled();
+  });
+
+  it('New Todo만 입력 시 ADD 버튼이 비활성화되고 있다.', () => {
+    // WHEN
+    fireEvent.change(todoFormText, { target: { value: '파운드 자르기' } });
+
+    // THEN
+    expect(todoFormButton).toBeDisabled();
+  });
+
+  it('활성화된 ADD 버튼 클릭 시 input 필드들이 초기화된다.', () => {
+    fireEvent.change(todoFormText, { target: { value: '파운드 접시에 담기' } });
+    fireEvent.change(todoFormDeadline, { target: { value: '2025-04-14' } });
+
+    act(() => {
+      fireEvent.click(todoFormButton);
+    });
+
+    expect(todoFormText).toHaveValue('');
+    expect(todoFormDeadline).toHaveValue('');
+  });
+})
--- zz.home-work/src/components/todo-list.tsx
+++ zz.home-work/src/components/todo-list.tsx
@@ -28,7 +28,8 @@ export const TodoList = ({
   return (
     <List style={{ marginTop: '2rem' }}>
       {todos.map((todo) => (
-        <ListItem key={todo.id}>
+        <ListItem key={todo.id}
+                  data-testid={`todo-list-item-${todo.id}`}>
           <Checkbox
             inputProps={
               {
@@ -52,6 +53,7 @@ export const TodoList = ({
             }}
           />
           <IconButton
+            data-testid={`delete-icon-button-${todo.id}`}
             edge="end"
             aria-label="delete"
             onClick={() => handleDeleteTodo(todo.id)}
